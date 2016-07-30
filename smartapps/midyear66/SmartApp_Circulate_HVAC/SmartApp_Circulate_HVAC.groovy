8fe5308c58eabd3f0c2e5dde1f43968f8fd6dcc8
deadmoo83 <deadmoo@gmail.com>
2016-07-29 21:04:31 -0400

    Update SmartApp_Circulate_HVAC.groovy

--- smartapps/midyear66/SmartApp_Circulate_HVAC/SmartApp_Circulate_HVAC.groovy
+++ smartapps/midyear66/SmartApp_Circulate_HVAC/SmartApp_Circulate_HVAC.groovy
@@ -14,7 +14,7 @@
  *
  */
 definition(
-    name: "SmartApp_Circulate_HVAC",
+    name: "Run circulate in HVAC - with virtual tile",
     namespace: "midyear66",
     author: "Bob Sanford",
     description: "Run circulate every X minutes if AC or heat has not been on",
